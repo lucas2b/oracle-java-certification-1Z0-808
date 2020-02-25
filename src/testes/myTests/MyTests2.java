5f2a971d3699ea4421eb391b28a828683710ff34
Lucas Barros Bonine <lucasbonine@live.com>
2020-02-25 18:35:26 -0300

    Atualização dos estudos

--- src/testes/myTests/MyTests2.java
+++ src/testes/myTests/MyTests2.java
@@ -1,23 +1,34 @@
 package testes.myTests;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Student{
-	public void metodo() {
-		System.out.println("metodo");
+	public String name;
+	
+	public void setName(String param) {
+		this.name = param;
 	}
 }
 
-
-public class MyTests2{
+public class MyTests2 {
 	
 	public static void main(String[] args) {
+		
+		List colors = new ArrayList();
+		colors.add("green");
+		colors.add("red");
+		colors.add("blue");
+		colors.add("yellow");
+		
+		colors.remove(2);
+		colors.add(3, "cyan");
+		
+		System.out.println(colors);
+		
+		
+		
 	
-		StringBuilder sb = new StringBuilder("a");
-		sb.append(sb);
-		
-		System.out.println(sb);
-		
-		
-		
-		
-	}	
+    }	
+	
 }
